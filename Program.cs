45d51025b27b0e46d0dd21331ee65849c4481bb1
safari05 <safari.erie@gmail.com>
2024-09-05 17:04:57 +0700

    update program.cs

--- Program.cs
+++ Program.cs
@@ -1,27 +1,57 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using TodoTask.Web.Data;
+using TodoTask.Web.Repositories.Todos;
+using TodoTask.Web.Repositories.User;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    opt.UseSqlite(connectionString);
+});
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ITodoRepository, TodoRepository>();
+
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddMemoryCache();
+
+// set sessions
+builder.Services.AddSession(opts =>
+{
+    opts.IdleTimeout = TimeSpan.FromSeconds(1000); // set hardcode time
+});
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Dashboard/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseSession();
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.Run();
